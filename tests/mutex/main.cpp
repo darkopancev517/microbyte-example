8b6822da1faf8ecb9af7f04adf0531ed344acf29
darkopancev517 <igededarkopancev@gmail.com>
2020-12-20 23:18:47 +0800

    update mutex test code

--- tests/mutex/main.cpp
+++ tests/mutex/main.cpp
@@ -5,27 +5,23 @@
 DigitalOut led1(PA_2);
 DigitalOut led2(PA_3);
 
-char thread1Stack[0x400];
 MicroByteMutex _mutex;
 
-void *thread1Handler(void *)
+void *thread1(void *)
 {
     while (1)
     {
         led2 = !led2;
         _mutex.lock();
     }
-    return NULL;
+    return nullptr;
 }
 
 int main()
 {
     _mutex = MicroByteMutex();
 
-    MicroByteThread::init(thread1Stack, sizeof(thread1Stack),
-            MICROBYTE_THREAD_PRIORITY_MAIN - 1,
-            MICROBYTE_THREAD_FLAGS_WOUT_YIELD | MICROBYTE_THREAD_FLAGS_STACKMARKER,
-            thread1Handler, NULL, "thread1");
+    MicroByteThread::init(thread1, "thread1", MICROBYTE_THREAD_PRIORITY_MAIN - 1);
 
     MicroByteScheduler::get().yield();
 
